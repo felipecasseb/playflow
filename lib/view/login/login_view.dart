820346d83d277f3d359f242ec27a7b68c8e1b601
Neto Modesto <Jnetomodesto9906@gmail.com>
2024-05-21 11:53:56 -0300

    test

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
 
@@ -20,24 +21,57 @@ class _LoginState extends State<LoginView> {
             flex: 40,
             child: Container(
               width: largura,
-            color: Colors.deepOrange,
-            child: Image.asset(
-              'imagens/Login_view1.png',
-            ),
+              color: Colors.deepOrange,
+              child: Image.asset(
+                'imagens/Login_view1.png',
+
+              ),
             ),
           ),
           Expanded(
             flex: 60,
             child: Container(
-              //width: largura,
+              width: largura,
               color: Colors.white,
               child: Column(
                 children: [
+                const SizedBox(height: 100),
                   Image.asset(
                       'imagens/Login_view2.png'),
-                  Text('Organize '),
+                const SizedBox(height: 30,),
+                  const Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 70),
+                    child: Text('Organize seus boletos em um só lugar', style: TextStyle(
+                    fontSize: 32,
+                      color: Color(0xFF585666)
+                    ),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+
+                    padding: const EdgeInsets.symmetric(horizontal: 40),
+                    child: Container(
+                      color: Colors.grey,
+                      height: 56,
+
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.email,
+                            ),
+                            Text('Entra com email'),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
-      
+
               ),
             ),
           ),
