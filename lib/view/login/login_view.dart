0ebca8c95592c08132611c1ffff534c5cd54c523
felipecasseb <felipe.casseb@hotmail.com>
2024-05-21 11:03:10 -0300

    Correção

--- lib/view/login/login_view.dart
+++ lib/view/login/login_view.dart
@@ -2,51 +2,47 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Login());
-}
-
-class Login extends StatefulWidget {
-  const Login({super.key});
+class LoginView extends StatefulWidget {
+  const LoginView({super.key});
 
   @override
-  State<Login> createState() => _LoginState();
+  State<LoginView> createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     double largura = MediaQuery.of(context).size.width;
-    return Column(
-      children: [
-        Expanded(
-          flex: 40,
-          child: Container(
-            width: largura,
-          color: Colors.deepOrange,
-          child: Image.asset(
-            'imagens/Login_view1.png',
-
-          ),
-          ),
-        ),
-        Expanded(
-          flex: 60,
-          child: Container(
-            //width: largura,
-            color: Colors.white,
-            child: Column(
-              children: [
-
-                Image.asset(
-                    'imagens/Login_view2.png'),
-                Text('Organize '),
-              ],
-
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            flex: 40,
+            child: Container(
+              width: largura,
+            color: Colors.deepOrange,
+            child: Image.asset(
+              'imagens/Login_view1.png',
+            ),
             ),
           ),
-        ),
-      ],
+          Expanded(
+            flex: 60,
+            child: Container(
+              //width: largura,
+              color: Colors.white,
+              child: Column(
+                children: [
+                  Image.asset(
+                      'imagens/Login_view2.png'),
+                  Text('Organize '),
+                ],
+      
+              ),
+            ),
+          ),
+        ],
+      ),
     );
 
 
--- lib/view/splash/splash_view.dart
+++ lib/view/splash/splash_view.dart
@@ -1,41 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(Myapp());
-
-}
-
-class Myapp extends StatefulWidget {
-  const Myapp({super.key});
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
 
   @override
-  State<Myapp> createState() => _MyappState();
+  State<SplashView> createState() => _SplashViewState();
 }
 
-class _MyappState extends State<Myapp> {
+class _SplashViewState extends State<SplashView> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: Color(0xFFFFFFFF),
-          body: Center(
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(height: 20.0),
-                Image.asset(
-                  'imagens/Logo_pay.png',
-                  width: 242,
-                  height: 374,
-                ),
-
-              ],
-            ),
-          ),
-        )
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("PayFlow"),
+      ),
     );
-
   }
 }
